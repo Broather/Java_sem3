039d93c5064a16f5517d3fd0646d7c4b9c4c4d00
Broather <markussgr@gmail.com>
2022-04-14 14:27:50 +0300

    added new Controller function

--- src/main/java/com/example/demo/controllers/SimpleController.java
+++ src/main/java/com/example/demo/controllers/SimpleController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +9,13 @@ public class SimpleController {
 @GetMapping("/simple") //localhost:8080/simple
 
 public String simpleFunc() {
-		System.out.println("Funkcija nostrādāja");
+		System.out.println("simple funkcija nostrādāja");
 		return "home";
+	}
+@GetMapping("/msg") //localhost:8080/msg
+	public String msgFunc(Model model) {
+		model.addAttribute("package", "ziņa no backend");
+		System.out.println("msg funkcijas nostrādāja");
+		return "msg-page";
 	}
 }
